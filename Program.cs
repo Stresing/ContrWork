6f44fc2cc4663df5ba659b56c9b04e6049b93ab5
stresing <Stresingrad@gmail.com>
2022-11-10 20:15:10 +0700

    first code attempt - not work(need) massiv strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(char[] collection) // вывод Массива
+{
+    Console.Write("[" + string.Join("", collection) + "]");
+}
+
+Console.WriteLine("Введите данные");
+string? str = Console.ReadLine(); // пользователь вводит данные цифры/слова + проверка на null
+char[] customArray = new char[str.Length]; //  Преобразовываем строку в массив с наименованием customArray.
+for (int i = 0; i < str.Length; i++)
+{
+    customArray[i] = str[i];
+}
+
+if (customArray.Length <= 3) // проверка на количество символов + 
+{
+    char[] limitArray = new char[customArray.Length];
+    for (int i = 0; i < customArray.Length; i++)
+    {
+        limitArray[i] = customArray[i];
+    }
+    PrintArray(limitArray);
+}
